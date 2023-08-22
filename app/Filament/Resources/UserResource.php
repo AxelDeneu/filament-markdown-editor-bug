c81f0ba582aac0096d071e0d0381f9406890f422
Axel Deneu <axel.deneu@gmail.com>
2023-08-22 14:59:25 +0200

    refactor(Initial-Commit): Add the code to reproduce the bug

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                MarkdownEditor::make('excerpt')
+                    ->hintAction(
+                        \Filament\Forms\Components\Actions\Action::make('generate')
+                            ->icon('heroicon-o-sparkles')
+                            ->hidden(fn () => $form->getOperation() !== 'edit')
+                            ->action(function (callable $set, MarkdownEditor $component) use ($form) {
+                                $excerpt = "But when I dynamically change it's value, with a `hintAction` for example, the input render, but it's unusable. To demonstrate, I will focus the input and press `space`. You'll se that the render is broken. If I press some keys, the behavior is not as expected.";
+
+                                $set('excerpt', $excerpt);
+                            })
+                    ),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
